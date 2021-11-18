d6ed0531f34b963b800c3c55dc042ffbe94fbe24
Nicole Martin <n.martinpdx@gmail.com>
2021-11-18 15:45:15 -0800

    add functional chrome ext working

--- public/background.js
+++ public/background.js
@@ -1,5 +1,7 @@
 /*global chrome*/
+let color = '#3aa757';
+
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Chrome extension successfully installed!');
-  return;
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
 });
