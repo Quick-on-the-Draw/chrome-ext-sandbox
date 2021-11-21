eb8922c1f8562997e476c044ab83b2aeb9c9a543
KB <katrinkajb@gmail.com>
2021-11-20 16:55:35 -0800

    working in React

--- public/background.js
+++
@@ -1,7 +0,0 @@
-/*global chrome*/
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { render } from 'react-dom';
 
 const Popup = () => {
-  return (
-    <div>
-      <h1>PopUp</h1>
-    </div>
-  );
+    return (
+        <main>
+            <h1>YOOOOO NICOLE</h1>
+            <button>Click</button>
+        </main>
+    );
 };
 
+render(<Popup />, document.getElementById('react-target'));
 export default Popup;
